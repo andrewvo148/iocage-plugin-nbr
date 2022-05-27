2010bd6b20ec1ff417b7c119dd89ba24ed0a9c99
Hiep Vo <53218461+andrewvo148@users.noreply.github.com>
2022-05-27 14:24:59 +0700

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -14,6 +14,7 @@ if [ $? -ne 0 -o ! -e $INSTALL ]; then
     exit 1
 fi
 
+
 # CHECKSUM=`sha256 -q $INSTALL`
 # if [ "$SHA256" != "$CHECKSUM" ]; then
 #     echo "ERROR: Incorrect $PRODUCT installer checksum"
