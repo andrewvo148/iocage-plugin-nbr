3137fd7348e571911d453245656a03ebaa38b5e9
Hiep Vo <53218461+andrewvo148@users.noreply.github.com>
2022-05-30 15:20:22 +0700

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 PRODUCT='NAKIVO Backup & Replication'
-URL="https://s3-nakivo-bucket.s3.ap-southeast-1.amazonaws.com/NAKIVO_Backup_Replication_v10.7.0.65630_Installer-TRIAL.sh"
+URL="https://download1327.mediafire.com/5wb3o6mwuhqg/ycl67ok1rbhca0n/NAKIVO_Backup_Replication_v10.7.0.65677_Installer-TRIAL.sh"
 SHA256="ddcf3aed10e22abac48987637fab0a7c25e776ec757dfaba227a0121ce7a73a2"
 
 PRODUCT_ROOT="/usr/local/nakivo"
